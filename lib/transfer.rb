46801bca9bd63a74248b4fa28b5c9508af8d70ef
Alec German <alec_german@yahoo.com>
2018-02-18 22:26:09 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    self.sender.valid? && self.receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+
+  end
+
 end
