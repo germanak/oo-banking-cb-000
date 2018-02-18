bb4a13c93a947b22f5480ba4c5d7893de1d3470e
Alec German <alec_german@yahoo.com>
2018-02-18 22:31:31 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
 
   def valid?
-    self.sender.valid? && self.receiver.valid? ? true : false
+    self.sender.valid? || self.receiver.valid? ? true : false
   end
 
   def execute_transaction
